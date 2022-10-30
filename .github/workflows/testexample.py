4736494f706a0845df04bd3d3767be9636a01171
OSAMA <109724330+OSA600@users.noreply.github.com>
2022-10-30 19:59:48 +0100

    update the py file with token variable

--- .github/workflows/testexample.py
+++ .github/workflows/testexample.py
@@ -1,5 +1,12 @@
+import os 
+
+
 def main():
   print ("c'est un test")
+  token = os.environ.get("DOCKER_SECRET_TOKEN")
+  if not token : 
+    raise RuntimeError("accès non autorisé, cette application va se fermer maintenant")
+  print ("bienvenue ")
   
 #sans cette ligne Github ne saura pas si le code est exécutable ou non
 if __name__ == '__main__':
